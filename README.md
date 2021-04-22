872d3821126ba37fbd70497c1d899f11ffc6bfac
Владислав Бахмацкий <astrax200469gmail.com@MacBook-Air-Vladislav.local>
2021-04-22 18:06:42 +0300

    started to code lexer

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Selenium-AutoTester
+
+file extension - *.sat
+
+Comannds list:
+    use [chromium / firefox] - start selenium with current driver
+    open [addres] - open this web browser
+    click [findmode] [selector] - click on current element with findmode
+    keys [findmode] [selector] [keys(list)] - insert values ​​into the desired field
+
+error code: 
+    104 - file empety or dosent exist
+    101 - unknown command
+    102 - argument exeption
--- parser/sat_lexer.py
+++ parser/sat_lexer.py
@@ -1,1 +1,35 @@
+import sys as s
+
 class Lexer:
+    def __init__(self, contents):
+        self.contents = contents
+
+        self.commands = []
+    
+    def read(self):
+        for string in self.contents:
+            self._parseString(string)
+        pass
+
+    def _error(self, msg, code):
+        print('lexer error: ' + msg)
+        return code
+        
+    #чекает всю строчку
+    def _parseString(self, string):
+        ## Исправить хрень с тем, что парсер работает криво
+        if (command_list[0] == 'use'):
+            if (len(command_list) != 2):
+                self._error('command use have only one fuckin argument!', 102)
+                pass
+            
+            if (command_list[1] != 'chromium' or command_list[1] != 'firefox'):
+                self._error('unknown argument!', 101)
+                pass
+
+        errMsg = 'unkown command >> ' + string[0] + ' << please read the fucking documentation before doing anything'
+        self._error(errMsg, 101)
+        pass
+
+lol = Lexer((['use chromium'], ['use firefox']))
+lol.read()
