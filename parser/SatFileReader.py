6267bd9826792001be4ad93449f57b80d541360b
A-ST-RA <astrax200469@gmail.com>
2021-04-22 22:46:46 +0300

    finaly edit file parsing

--- parser/SatFileReader.py
+++ parser/SatFileReader.py
@@ -8,20 +8,29 @@ class Reader:
         file = open(self.path, 'r')
 
         if (file.name.split('.')[1] != 'sat'):
-            self._error('unknown file extension, the script must have the .sat extension.', 103)
-            pass
+            return self._error('unknown file extension, the script must have the .sat extension!', 103)
 
-        for el in file:
-            commands.append(el)
+        for string in file:
+            el = string.replace('\n', ' ')
+            el = el.split(' ')
+            
+            _command = []
+            for lex in el: 
+                if (lex != ''):
+                    _command.append(lex)
+            if (len(_command) > 0):
+                commands.append(_command)
+
+        if (len(commands) <= 0):
+            return self._error('file empety', 104)
 
         file.close()
         return commands
 
+    def _clear(self):
+        pass
+
+
     def _error(self, msg, code):
         print('file reader error: ' + msg)
         return code
-        
-
-reader = Reader('test.sat')
-print(reader.readFile())
-
--- parser/SatLexer.py
+++ parser/SatLexer.py
@@ -1,33 +1,30 @@
 import sys as s
 
 class Lexer:
-    def __init__(self, contents):
+    def __init__(self, *contents):
         self.contents = contents.split(' ')
         self.commands = []
-    
+
     def read(self):
         for string in self.contents:
             print(string)
 
-    def _error(self, msg, code):
-        print('lexer error: ' + msg)
-        return code
-        
     #чекает всю строчку
     def _parseString(self, string):
         ## Исправить хрень с тем, что парсер работает криво
         if (command_list[0] == 'use'):
             if (len(command_list) != 2):
-                self._error('command use have only one fuckin argument!', 102)
-                pass
+                return self._error('command use have only one fuckin argument!', 102)
             
             if (command_list[1] != 'chromium' or command_list[1] != 'firefox'):
-                self._error('unknown argument!', 101)
-                pass
+                return self._error('unknown argument!', 101)
 
         errMsg = 'unkown command >> ' + string[0] + ' << please read the fucking documentation before doing anything'
-        self._error(errMsg, 101)
-        pass
+        return self._error(errMsg, 101)
+
+    def _error(self, msg, code):
+        print('lexer error: ' + msg)
+        return code
 
 lol = Lexer('use chromium use firefox')
 lol.read()
